184536ca1546121cc467a60c036f7b46f3040142
Brandon J <splintercel3000@gmail.com>
2020-04-21 16:24:30 -0700

    push

---
+++ challenges/splitsort/splitssort/splitssort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace splitssort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

773baf96f3fd32dac2822a7cea24a8930c83c0b0
Brandon J <splintercel3000@gmail.com>
2020-05-11 10:05:42 -0700

    done comments for the methods

---
+++ data-structures/graphs/GraphImplD10/Classes/Edge.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphImplD10.Classes
+{/// <summary>
+/// set up for edge class
+/// </summary>
+/// <typeparam name="T"></typeparam>
+    class Edge<T>
+    {
+        public int Weight { get; set; }
+        public Vertex<T> Vertex { get; set; }
+    }
+}
---
+++ data-structures/graphs/GraphImplD10/Classes/Vertex.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphImplD10.Classes
+{
+    // This was class 08!!!!
+    /// <summary>
+    /// This is our set up for vertexes
+    /// </summary>
+    /// <typeparam name="Potato"></typeparam>
+    class Vertex<Potato>
+    {
+        public Potato Value { get; set; }
+
+        public Vertex(Potato value)
+        {
+            Value = value;
+        }
+    }
+}
